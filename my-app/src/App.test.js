c2e1b3e962149e7220502537db4fa978239cf60a
Damian Todorov <damqn_dimitrov1111@icloud.com>
2023-12-18 23:12:09 +0200

    one test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { expect } from "vitest";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Should have Worldwide delivery!", () => {
+  render();
+
+  const message = screen.queryByText(/Worldwide delivery!/i);
+  expect(message).toBeDefined();
 });
+
